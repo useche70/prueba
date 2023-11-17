46624a4504f2283b56d9ea3637cdd8ac9acb0f51
useche70 <151178859+useche70@users.noreply.github.com>
2023-11-17 15:33:56 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano151123",
+      "name": "pano151123",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.385271145892748,
+        "pitch": -0.4837012385319355,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2311890761921838,
+          "pitch": -0.04956870484090814,
+          "rotation": 0,
+          "target": "1-pano151123_03"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6659315347324526,
+          "pitch": -0.3747070834768955,
+          "title": "Teatro",
+          "text": "Opera de Nurenberg"
+        }
+      ]
+    },
+    {
+      "id": "1-pano151123_03",
+      "name": "pano151123_03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4524424506482987,
+        "pitch": -0.3469539752147366,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8683073059955486,
+          "pitch": -0.018170173691292035,
+          "rotation": 0,
+          "target": "0-pano151123"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
